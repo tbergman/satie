f90182c602a2e5174c29c919d5d9c152d0ea50b3
Josh Netterfield <joshua@nettek.ca>
2014-06-30 00:44:59 -0400

    Responsiveness.

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,13 @@ var Ripieno = require("./ripieno.jsx");
 
 global.React = React; // for Chrome devtool extension
 
+if (typeof window !== "undefined" &&
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
+            .test(navigator.userAgent)) {
+    React.initializeTouchEvents(true);
+    require("./landing/touch.less");
+}
+
 React.renderComponent(
     <Ripieno />,
     document.body
