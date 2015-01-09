a92411d364ca28a86578ef8b940b4560faeba316
Josh Netterfield <joshua@nettek.ca>
2015-01-09 18:52:06 -0500

    Add missing typing files.

---
+++ website/typings/react-invariant.d.ts
@@ -0,0 +1,4 @@
+declare module 'react/lib/invariant' {
+    function invariant(condition: boolean, format: string, param1?: any, param2?: any, param3?: any, param4?: any, param5?: any, param6?: any): void;
+    export = invariant;
+}
